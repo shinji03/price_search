bd4a16ee136d07ca870155ea653a98db0c92a6d6
shinji.yamamoto <daytodaytodaytogo@gmail.com>
2022-09-10 22:57:20 +0900

    商品の追加ページの作成

--- src/main/java/com/example/demo/controllers/products/ProductsController.java
+++ src/main/java/com/example/demo/controllers/products/ProductsController.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.forms.ProductsForm;
 import com.example.demo.models.Category;
 import com.example.demo.models.Products;
 import com.example.demo.repositories.ProductsRepository;
@@ -99,19 +102,93 @@ public class ProductsController {
     }
 
     /**
-     * 商品の登録
+     * 商品の登録ページに遷移
+     * @param model Model
+     * @return 検索結果一覧画面のHTML
+     */
+
+    @RequestMapping(value = "/products/new", method = RequestMethod.GET)
+    public ModelAndView displayNew(@ModelAttribute ProductsForm productsForm, ModelAndView mv) {
+
+        //カテゴリーのリストの作成
+        List<Category> Categorylist = categoryService.searchAll();
+        mv.addObject("categoryList", Categorylist);
+
+        mv.addObject("product", productsForm);
+        mv.setViewName("views/products/new.html");
+
+        return mv;
+    }
+
+    /**
+     * 商品を登録する
      * @param model Model
      * @return 検索結果一覧画面のHTML
      */
 
     @RequestMapping(value = "/products/create", method = RequestMethod.GET)
-    public String displayCreate(Model model) {
+    public String displayCreate() {
 
-        //カテゴリーのリストの作成
-        List<Category> Categorylist = categoryService.searchAll();
-        model.addAttribute("categoryList", Categorylist);
-
-        return "views/products/create.html";
+        return "views/products/list.html";
     }
+
+    /*
+
+// 作成画面に遷移する内容の書き方
+    @RequestMapping(path = "/reports/new", method = RequestMethod.GET)
+    public ModelAndView reportsNew(@ModelAttribute ReportForm reportForm, ModelAndView mv) {
+
+        reportForm.setToken(session.getId());
+        reportForm.setReportDate(new Date(System.currentTimeMillis()));
+
+        mv.addObject("report", reportForm);
+
+        mv.setViewName("views/reports/new");
+
+        return mv;
+    }
+    //作成のやり方
+    @RequestMapping(path = "/reports/create", method = RequestMethod.POST)
+    @Transactional // メソッド開始時にトランザクションを開始、終了時にコミットする
+    public ModelAndView reportsCreate(@ModelAttribute ReportForm reportForm, ModelAndView mv) {
+
+        if (reportForm.getToken() != null && reportForm.getToken().equals(session.getId())) {
+
+            Report r = new Report();
+            r.setEmployee((Employee)session.getAttribute("login_employee"));
+
+            Date report_date = new Date(System.currentTimeMillis());
+            if(reportForm.getReportDate() != null) {
+                report_date =reportForm.getReportDate();
+            }
+            r.setReportDate(report_date);
+
+            r.setTitle(reportForm.getTitle());
+            r.setContent(reportForm.getContent());
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            r.setCreated_at(currentTime);
+            r.setUpdated_at(currentTime);
+
+            List<String> errors = validator.validate(r);
+
+            if (errors.size() > 0) {
+                reportForm.setToken(session.getId());
+                mv.addObject("report", reportForm);
+                mv.addObject("errors", errors);
+                mv.setViewName("views/reports/new");
+            } else {
+                reportRepository.save(r);
+                session.setAttribute("flush", "登録が完了しました。");
+
+                mv = new ModelAndView("redirect:/reports/index"); // リダイレクト
+            }
+        } else {
+            mv.setViewName("views/common/error"); // 真っ白な画面は嫌なので、エラー画面を出す
+        }
+
+        return mv;
+    }
+*/
 
 }
--- src/main/java/com/example/demo/forms/ProductsForm.java
+++ src/main/java/com/example/demo/forms/ProductsForm.java
@@ -6,12 +6,22 @@ public class ProductsForm extends Products {
 
     private String proName;
 
+    private String token;
+
     public String getProName() {
         return proName;
     }
 
     public void setProName(String proName) {
         this.proName = proName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 
 }
